67715ae37a5ad07d68e915fb18c665e5cec634d7
Chris Haggerty <chris.m.haggerty@gmail.com>
2016-01-13 21:54:26 -0500

    adds list cross-referencing algorithm

--- best_players_available.py
+++ best_players_available.py
@@ -1,5 +1,6 @@
 from lxml import html
 import requests
+import string
 page = requests.get('http://www.pgatour.com/stats/stat.186.html') #player rankings
 tree = html.fromstring(page.text)
 players = tree.xpath('//*[contains(@id, "playerStatsRow")]/td[3]/a/text()')
@@ -7,8 +8,17 @@ page2 = requests.get('http://www.pgatour.com/tournaments/sony-open-in-hawaii/fie
 tree2 = html.fromstring(page2.content)
 field = tree2.xpath('//div[@class="field-table-content clearfix"]/div/p/text()')
 
-top_players = players[0:99] #top 100
+lineup = []
 
-#now need to figure out how to determine intersection of these lists, which is non-trivial since the name formatting is different
-
-
+for guy in players:
+	temp = string.split(guy)
+	guys_last_name = temp[1]
+	for f in field:
+		if guys_last_name in f:
+			lineup.append(f)
+			if len(lineup) ==7:
+				print(lineup)
+				continue
+			 
+	
+#next: add ranking of each player to the lineup as a check
