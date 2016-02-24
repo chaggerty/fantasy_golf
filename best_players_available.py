039ca5340af624cac9468ffab2e35d2929f97620
Chris Haggerty <chris.m.haggerty@gmail.com>
2016-02-23 22:29:48 -0500

    adds input argument for tournament name

--- best_players_available.py
+++ best_players_available.py
@@ -1,13 +1,21 @@
 from lxml import html
+import sys
 import requests
 import string
+
+for arg in sys.argv[1:]:
+    if arg == '-h':
+        print 'best_players_available.py -i string-of-tourny-name'
+        sys.exit()
+    elif arg in ("-i","--ifile"):
+	inp = sys.argv[2]
 page = requests.get('http://www.pgatour.com/stats/stat.186.html') #player rankings
 tree = html.fromstring(page.text)
 players = tree.xpath('//*[contains(@id, "playerStatsRow")]/td[3]/a/text()')
 page1 = requests.get('http://www.pgatour.com/stats/stat.02671.html') #current FedEx Cup rankings
 tree = html.fromstring(page1.text)
 fedex = tree.xpath('//*[contains(@id, "playerStatsRow")]/td[3]/a/text()')
-page2 = requests.get('http://www.pgatour.com/tournaments/careerbuilder-challenge-in-partnership-with-the-clinton-foundation/field.html') #update with current tournament
+page2 = requests.get('http://www.pgatour.com/tournaments/'+ inp +'/field.html') 
 tree2 = html.fromstring(page2.content)
 field = tree2.xpath('//div[@class="field-table-content clearfix"]/div/p/text()')
 
