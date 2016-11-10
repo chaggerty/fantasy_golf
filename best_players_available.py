64a8ac27789243907549221b810beb2a50bfad7d
Chris Haggerty <chris.m.haggerty@gmail.com>
2016-11-09 21:00:28 -0500

    tournament field now listed on json file on pga site, function updated accordingly

--- best_players_available.py
+++ best_players_available.py
@@ -1,23 +1,21 @@
+#!/usr/local/bin/python
+
 from lxml import html
 import sys
 import requests
 import string
+import json
 
-for arg in sys.argv[1:]:
-    if arg == '-h':
-        print 'best_players_available.py -i string-of-tourny-name'
-        sys.exit()
-    elif arg in ("-i","--ifile"):
-	inp = sys.argv[2]
 page = requests.get('http://www.pgatour.com/stats/stat.186.html') #player rankings
 tree = html.fromstring(page.text)
 players = tree.xpath('//*[contains(@id, "playerStatsRow")]/td[3]/a/text()')
 page1 = requests.get('http://www.pgatour.com/stats/stat.02671.html') #current FedEx Cup rankings
 tree = html.fromstring(page1.text)
 fedex = tree.xpath('//*[contains(@id, "playerStatsRow")]/td[3]/a/text()')
-page2 = requests.get('http://www.pgatour.com/tournaments/'+ inp +'/field.html') 
-tree2 = html.fromstring(page2.content)
-field = tree2.xpath('//div[@class="field-table-content clearfix"]/div/p/text()')
+page2 = requests.get('http://www.pgatour.com/data/r/457/field.json')
+tree2 = page2.json()
+field = tree2['Tournament']['Players']
+
 
 lineup = []
 print("Based on world rankings:")
@@ -26,21 +24,21 @@ for guy in players:
 	last_name = temp[1]
 	first_name = temp[0]
 	for f in field:
-		if last_name in f and first_name in f: 
-			lineup.append(f)
+		if last_name in f['PlayerName'] and first_name in f['PlayerName']: 
+			lineup.append(str(f['PlayerName']))
 			if len(lineup) ==7:
 				print(lineup)
 			break	
 			 
-print("Based on FedEx Cup Rankings:")
+print("\nBased on FedEx Cup Rankings:")
 lineup = []
 for guy in fedex:
 	temp = string.split(guy)
 	last_name = temp[1]
 	first_name = temp[0]
 	for f in field:
-		if last_name in f and first_name in f: 
-			lineup.append(f)
+		if last_name in f['PlayerName'] and first_name in f['PlayerName']: 
+			lineup.append(str(f['PlayerName']))
 			if len(lineup) ==7:
 				print(lineup)
 			break
